ffb6205f4c1b785a8a59dde7a9688d0f947e15f3
Maxim Kazantsev <aeontal@gmail.com>
2021-04-04 02:22:20 +0500

    first commit

---
+++ src/test/java/PostmanQueryTest.java
@@ -0,0 +1,34 @@
+import io.restassured.http.ContentType;
+
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class PostmanQueryTest {
+
+
+    @Test
+    public void postmanTestPostRequest(){
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Проверка") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("проверка"))
+        ;
+
+
+    }
+
+}
